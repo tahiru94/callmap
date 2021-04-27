031ef551827acab110c94f27499d06f905935d2e
tahiru94 <tahiru1994@engineer.com>
2021-04-26 21:53:41 -0400

    Update default / base components for routes (WIP)

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,29 +4,29 @@ const AppRoutes: IRoute[] = [
     {
         path: '/',
         sidebarName: 'Dashboard',
-        component: <div>Home</div>,
+        component: () => <div>Home</div>,
         showInSidebar: true,
     },
     {
         path: '/new',
         sidebarName: 'Add Callmap Record',
-        component: <div>Add Callmap Record</div>,
+        component: () => <div>Add Callmap Record</div>,
         showInSidebar: true,
     },
     {
         path: '/edit/:id',
-        component: <div>Edit Callmap Record</div>,
+        component: () => <div>Edit Callmap Record</div>,
         showInSidebar: false,
     },
     {
         path: '/view/:id',
-        component: <div>View Callmap Record</div>,
+        component: () => <div>View Callmap Record</div>,
         showInSidebar: false
     },
     {
         path: '/all',
         sidebarName: 'Historic View',
-        component: <div>Historic View</div>,
+        component: () => <div>Historic View</div>,
         showInSidebar: true
     },
 ];
