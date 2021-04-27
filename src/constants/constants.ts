1d6f2fb56f8a319f89bb19a977e27b420d3230ed
tahiru94 <tahiru1994@engineer.com>
2021-04-27 12:22:20 -0400

    Add EditCallmapContainer and integrate with routing
    * Add EditCallmapContainer for displaying edit form

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -4,4 +4,4 @@ export const CALLMAP_API_BASE_URL = `${BASE_URL}/v1/callmap-record`;
 export const CALLMAP_API_GET_LATEST_URL = `${CALLMAP_API_BASE_URL}/latest`;
 export const CALLMAP_API_GET_ALL_URL = `${CALLMAP_API_BASE_URL}/all`;
 
-export const CALLMAP_API_OPERATION_BY_ID_URL = `${CALLMAP_API_BASE_URL}/id/`;
+export const CALLMAP_API_OPERATION_BY_ID_URL = `${CALLMAP_API_BASE_URL}/id`;
